583723276ee8ee3ba6cf962dece540efafe8ad32
Reuben Garrett <ruby+dev@rubydono.net>
2016-06-01 03:36:11 -0500

    Add Node.js glue
    Initialized Node.js project with Mocha and Chai
    for testing via Travis CI.

---
+++ test/test.js
@@ -0,0 +1,21 @@
+// https://www.codementor.io/nodejs/tutorial/unit-testing-nodejs-tdd-mocha-sinon
+
+// tests/part1/cart-summary-test.js
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+
+//var basicTest = require.main.require('impl/test.js');
+
+//var CartSummary = require('./../../src/part1/cart-summary');
+
+describe('alwaysSucceeds', function() {
+    it('alwaysSucceeds() should always suceed', function() {
+	expect(true).to.equal(true);
+    });
+});
+
+describe('alwaysFails', function() {
+    it('alwaysFails() should always fail', function() {
+	expect(true).to.equal(false);
+    });
+});
